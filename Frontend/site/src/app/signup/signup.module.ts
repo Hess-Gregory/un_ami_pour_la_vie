4ba1e63c40b08c1d42e903e67e915aea78dbfc6b
Gregory <gsweet1981@live.be>
2019-11-08 23:29:58 +0100

    Add module "register"
    Signed-off-by: Gregory <gsweet1981@live.be>

--- Frontend/site/src/app/signup/signup.module.ts
+++ Frontend/site/src/app/signup/signup.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
-
+import { MatButtonModule, MatCheckboxModule, MatSelectModule,  MatDatepickerModule,
+    MatNativeDateModule, MatInputModule } from '@angular/material';
 @NgModule({
   imports: [
     CommonModule,
     TranslateModule,
-    SignupRoutingModule
+    SignupRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
-  declarations: [SignupComponent]
+  declarations: [SignupComponent],
+  exports: [
+    TranslateModule
+  ]
 })
 export class SignupModule { }
