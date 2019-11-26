7592e8ee1f1e70302eaeb60c049e548e082d2fa0
Gregory <gsweet1981@live.be>
2019-11-26 03:03:15 +0100

    Timer de la navbar

--- Frontend/site/src/app/home/components/timer/timer.component.ts
+++ Frontend/site/src/app/home/components/timer/timer.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import * as jwt_decode from 'jwt-decode';
+import { AuthService } from './../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-timer',
@@ -9,40 +10,50 @@ import * as jwt_decode from 'jwt-decode';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TimerComponent implements OnInit {
-    deltaTime: any;
-    deltaTimeMessage;
+    deltaTime: Number;
+    deltaTimeMessage: any;
     router: any;
     currentDate: any;
     date333: any;
+    current_time: number;
+    expirationTime: number;
+    auth: any;
 
-  constructor() {
+  constructor(auth: AuthService) {
+}
+
+  ngOnInit() {
 
     const jwtToken = localStorage.getItem('access_token');
     if (jwtToken) {
-            const decoded = jwt_decode(jwtToken);
-            const expirationTime = decoded.exp;
-            new Date().toString();
-            const date = new Date(0);
-            date.setUTCSeconds(expirationTime);
-            date.valueOf();
-            const current_time2 = Date.now() / 1000;
-            const current_time =  Math.round(current_time2);
 
-                if (current_time  < expirationTime) {
+            const token = localStorage.getItem('access_token');
+            const expirationTime = jwt_decode(token)['exp'];
+            const current_time2: any  = Date.now() / 1000;
+            const current_time: any =  Math.round(current_time2);
+            this.currentDate = moment(new Date());
 
-                    this.currentDate = moment(new Date());
+              if (current_time < expirationTime) {
                     this.date333 = new Date( parseInt(expirationTime) * 1000 );
-                    this.deltaTime = Math.round((this.date333 - this.currentDate + 30000) / 1000 / 60);
-                    this.deltaTimeMessage = ' Votre connexion expirera dans ' + this.deltaTime + ' min';
+                    this.deltaTime = Math.round((this.date333  - this.currentDate + 30000) / 1000 / 60);
+                    this.deltaTimeMessage = ' La connexion expirera dans ' + this.deltaTime + ' min';
                     console.log(this.deltaTimeMessage);
-
-                 } else {
-                    this.deltaTimeMessage = ' Votre connexion a expiré.';
-                     console.log(this.deltaTimeMessage);
+                    return true;
+                } else {
+                  this.deltaTimeMessage = 'La connexion a expiré';
+                  console.log(this.deltaTimeMessage);
+                  this.router.navigate(['/login']);
+                  this.auth.logout();
+                  return false;
                 }
-    }
-  }
 
-  ngOnInit() { }
+      } else {
+        this.deltaTimeMessage = 'La connexion a expiré';
+        console.log(this.deltaTimeMessage);
+        this.router.navigate(['/login']);
+        return false;
+      }
+   }
 
 }
+
