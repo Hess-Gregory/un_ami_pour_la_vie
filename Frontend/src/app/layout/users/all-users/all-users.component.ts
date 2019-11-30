127dd90657159c1f11b7443275b49b2200047aa7
Gregory <gsweet1981@live.be>
2019-11-30 02:37:48 +0100

    Table des utlisateurs

--- Frontend/src/app/layout/users/all-users/all-users.component.ts
+++ Frontend/src/app/layout/users/all-users/all-users.component.ts
@@ -1,9 +1,9 @@
 import { AllUsersService } from './all-users.service';
-import { Component, OnDestroy, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import {  Subject } from 'rxjs';
-import { DataTableDirective } from 'angular-datatables';
 import { routerTransition } from '../../../router.animations';
-
+import { DataTableDirective } from 'angular-datatables';
+import { Router} from '@angular/router';
 declare let $: any;
 
 export interface Error {
@@ -22,24 +22,10 @@ export interface Error {
 
 export class AllUsersComponent implements OnDestroy,  OnInit {
 
-    public objStringError: string;
-    public objError: any;
-    public ErrorstatusText: any;
-    public Errormessage: string;
-    public ErrorStatus: string;
-    public error: Error;
-    alerts: Array<any> = [];
 
-    @ViewChild(DataTableDirective, {static: false})
-    datatableElement: DataTableDirective;
-    users: any = [];
-    dtOptions: DataTables.Settings = {};
-    dtTrigger: Subject<any> = new Subject();
-
-    constructor(private allusersservice: AllUsersService) {
+    constructor(private allusersservice: AllUsersService, private router: Router) {
 
         this.getUsers();
-
         this.alerts.push( {
             id: 3,
             type: 'danger',
@@ -47,22 +33,35 @@ export class AllUsersComponent implements OnDestroy,  OnInit {
             });
      }
 
-    ngOnInit() {
-     }
+    [x: string]: any;
+    public objStringError: string;
+    public objError: any;
+    public ErrorstatusText: any;
+    public Errormessage: string;
+    public ErrorStatus: string;
+    public error: Error;
+    users: any = [];
+    dataSource = this.users;
+    dtTrigger: Subject<any> = new Subject();
+    alerts: Array<any> = [];
+    dtOptions: DataTables.Settings = {};
+    datatableElement: DataTableDirective;
+
+    @ViewChild(DataTableDirective, {static: false})
+
+
+    ngOnInit() {}
 
     getUsers() {
       this.dtOptions = {
           pagingType: 'full_numbers',
           pageLength: 10
         };
-
       this.allusersservice.getUsers().subscribe(
-
         response => {
             this.users = response;
             this.dtTrigger.next();
             },
-
         error => {
             this.objStringError = JSON.stringify(error);
             this.objError = JSON.parse(this.objStringError);
@@ -82,4 +81,11 @@ export class AllUsersComponent implements OnDestroy,  OnInit {
         const index: number = this.alerts.indexOf(alert);
         this.alerts.splice(index, 1);
         }
+
+        RowSelected(user: any) {
+            this.data = user.id;
+            sessionStorage.setItem('idSelect', this.data);
+            this.router.navigate(['admin/users/get-user']);
+          }
+
 }
