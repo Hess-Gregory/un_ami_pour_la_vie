5968f061d73b99fe5ce5d7a1a576a00b13774b94
Gregory <gsweet1981@live.be>
2019-11-30 02:35:55 +0100

    Page admin get details user

--- Frontend/src/app/layout/users/all-users/get-user/get-user.component.ts
+++ Frontend/src/app/layout/users/all-users/get-user/get-user.component.ts
@@ -1,6 +1,21 @@
 import { GetUserService } from './get-user.service';
-import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../../../router.animations';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../../users-export';
+import {of} from 'rxjs';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {  Subject } from 'rxjs';
+declare let $: any;
+
+export interface Error {
+    objStringError: string;
+    objError: any;
+    ErrorstatusText: any;
+    Errormessage: string;
+    ErrorStatus: string;
+  }
+
 
 @Component({
   selector: 'app-get-user',
@@ -8,12 +23,86 @@ import { routerTransition } from '../../../../router.animations';
   styleUrls: ['./get-user.component.scss'],
   animations: [routerTransition()]
 })
-export class GetUserComponent implements OnInit {
+export class GetUserComponent  implements  OnDestroy,  OnInit {
 
-  constructor(private getuserservice: GetUserService) { }
+    locked = true;
+    [x: string]: any;
+    public objStringError: string;
+    public objError: any;
+    public ErrorstatusText: any;
+    public Errormessage: string;
+    public ErrorStatus: string;
+    public error: Error;
+    users: Array<any> = [];
 
-  ngOnInit() {
-  }
+    dtTrigger: Subject<any> = new Subject();
+    alerts: Array<any> = [];
 
+    constructor(private userservice: GetUserService) {
+        this.getUsers();
+        this.getStatus();
+        this.alerts.push( {
+            id: 3,
+            type: 'danger',
+            message: 'this.Errormessage',
+            });
+
+    }
+
+    ngOnInit() {
+            console.log('locked is : ', this.locked);
+
+    }
+
+onUnlock() {this.locked = false; }
+onSave() {this.locked = true; }
+onCancel() {this.locked = true; }
+onDelete() {this.locked = true; }
+onClose() {this.locked = true; }
+
+    getUsers() {
+
+        this.userservice.getUsers().subscribe(
+          response => {
+              this.users = response;
+
+              this.dtTrigger.next();
+              },
+          error => {
+              this.objStringError = JSON.stringify(error);
+              this.objError = JSON.parse(this.objStringError);
+              this.ErrorStatus = this.objError.status;
+              this.ErrorstatusText = this.objError.statusText;
+              this.Errormessage = this.objError.message;
+              console.log(error);
+              }
+        );
+      }
+      getStatus() {
+
+        this.userservice.getStatus().subscribe(
+          response => {
+              this.status = response;
+
+              this.dtTrigger.next();
+              },
+          error => {
+              this.objStringError = JSON.stringify(error);
+              this.objError = JSON.parse(this.objStringError);
+              this.ErrorStatus = this.objError.status;
+              this.ErrorstatusText = this.objError.statusText;
+              this.Errormessage = this.objError.message;
+              console.log(error);
+              }
+        );
+      }
+      ngOnDestroy(): void {
+        this.dtTrigger.unsubscribe();
+      }
+
+    public closeAlert(alert: any) {
+        const index: number = this.alerts.indexOf(alert);
+        this.alerts.splice(index, 1);
+        }
 }
 
--- Frontend/src/app/layout/users/all-users/get-user/get-user.service.ts
+++ Frontend/src/app/layout/users/all-users/get-user/get-user.service.ts
@@ -1,16 +1,24 @@
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../../users-export';
-
 @Injectable({
   providedIn: 'root'
 })
 export class GetUserService {
-
+    userid: string;
+    users: any;
+    userdetail: any;
 
   constructor(private http: HttpClient) { }
 
+
   getUsers() {
-    return this.http.get<User[]>('/api/users');
+    this.userid = sessionStorage.getItem('idSelect');
+    return this.http.get<User[]>(`/api/users/${this.userid}`);
+  }
+  getStatus() {
+    this.userid = sessionStorage.getItem('idSelect');
+    return this.http.get<User[]>(`/api/users/status/${this.userid}`);
   }
 }
