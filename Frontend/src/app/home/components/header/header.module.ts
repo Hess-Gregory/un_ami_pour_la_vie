4be0a926f198109853d593b42b280227338d2f8b
Gregory <gsweet1981@live.be>
2019-12-02 04:46:12 +0100

    correction erreurs home page

--- Frontend/src/app/home/components/header/header.module.ts
+++ Frontend/src/app/home/components/header/header.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderHomepageComponent } from './header.component';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { HeaderHomepageComponent } from './header.component';
 
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [HeaderHomepageComponent]
-})
-export class HeaderModule { }
+// @NgModule({
+//   imports: [
+//     CommonModule
+//   ],
+//   declarations: [HeaderHomepageComponent]
+// })
+// export class HeaderModule { }
