6f3286468f675e60d0b4f68d37b54447b9e6de79
Gregory <gsweet1981@live.be>
2019-12-13 10:51:28 +0100

    detail des membres

--- Frontend/src/app/layout/users/users-manager/user-detail/user-detail.module.ts
+++ Frontend/src/app/layout/users/users-manager/user-detail/user-detail.module.ts
@@ -9,6 +9,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { UserDetailComponent } from './user-detail.component';
 import { UserDetailRoutingModule } from './user-detail-routing.module';
+import { UserControlsModule } from '../user-controls/user-controls.module';
 
 @NgModule({
     imports: [
@@ -24,7 +25,8 @@ import { UserDetailRoutingModule } from './user-detail-routing.module';
         MatNativeDateModule,
         MatCheckboxModule,
         MatSelectModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        UserControlsModule
 
     ],
     declarations: [
