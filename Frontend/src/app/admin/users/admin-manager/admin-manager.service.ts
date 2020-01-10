731cf7a2e48e40eaaba27dd4337e3ed83a1a101c
Gregory <gsweet1981@live.be>
2020-01-10 15:29:19 +0100

    List admin - Module Finished - v.1.2.0

--- Frontend/src/app/admin/users/admin-manager/admin-manager.service.ts
+++ Frontend/src/app/admin/users/admin-manager/admin-manager.service.ts
@@ -1,30 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Admin, User } from './../services/users-export';
+import { User } from '../services/users-export';
 import { Role } from './../../../shared/exports';
 import { Router } from '@angular/router';
-import * as jwt_decode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminManagerService {
-  stringifydata: string;
-  parsedata: any;
+    private wsUrlRoot = '/api/admins/userandrole';
+    private wsUrlRoot2 = '/api/users/role';
+    stringifydata: string;
+    parsedata: any;
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
-  getAdmins() {
-    return this.http.get<Admin[]>('/api/admins/adminlist');
+  public getUsers() {
+    return this.httpClient.get<User[]>(`${this.wsUrlRoot}`);
   }
-  getUsers() {
-    return this.http.get<User[]>('/api/users/status');
+  public getRoles() {
+    return this.httpClient.get<Role[]>(`${this.wsUrlRoot2}`);
   }
-  getRoles() {
-    return this.http.get<Role[]>('/api/users/role');
+  public GetAllRecords(): Observable<Object> {
+    return this.httpClient.get<User[]>(`${this.wsUrlRoot}`);
   }
   update(userid: number, roleid: number) {
-    this.http
+    this.httpClient
       .put(`/api/users/${userid}`, {
         role: roleid
       })
