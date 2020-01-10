44cd542d39366def84ce92c1fb59ead6377e48ae
Gregory <gsweet1981@live.be>
2020-01-10 10:29:26 +0100

    update

---
+++ Frontend/src/app/admin/users/services/users-export.ts
@@ -0,0 +1,18 @@
+export interface User {
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  username?: string;
+  isActive?: string;
+  roleName?: string;
+}
+export interface Admin {
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  username?: string;
+  roleName?: string;
+  isActive?: string;
+}
