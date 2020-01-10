d8ba707430c3809db514cd556d5601296bf99aa9
Gregory <gsweet1981@live.be>
2020-01-10 10:35:09 +0100

    commit (these modules are to be removed soon)

---
+++ Frontend/src/app/admin/dashboard/dashboard.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
+import { Title, Meta } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [routerTransition()]
+})
+export class DashboardComponent implements OnInit {
+  public alerts: Array<any> = [];
+  public sliders: Array<any> = [];
+  title = 'Un Ami Pour La Vie - Admin Dashboard';
+
+  constructor(private titleService: Title, private metaTagService: Meta) {
+    this.sliders.push(
+      {
+        imagePath: 'assets/images/slider1.jpg',
+        label: 'First slide label',
+        text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+      },
+      {
+        imagePath: 'assets/images/slider2.jpg',
+        label: 'Second slide label',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      },
+      {
+        imagePath: 'assets/images/slider3.jpg',
+        label: 'Third slide label',
+        text: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+      }
+    );
+
+    this.alerts.push(
+      {
+        id: 1,
+        type: 'success',
+        message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+      },
+      {
+        id: 2,
+        type: 'warning',
+        message: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptates est animi quibusdam praesentium quam, et perspiciatis,
+                consectetur velit culpa molestias dignissimos
+                voluptatum veritatis quod aliquam! Rerum placeat necessitatibus, vitae dolorum`
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaTagService.updateTag({
+      name: 'description',
+      content: 'Un Ami Pour La Vie - Admin Dashboard'
+    });
+  }
+
+  public closeAlert(alert: any) {
+    const index: number = this.alerts.indexOf(alert);
+    this.alerts.splice(index, 1);
+  }
+}
