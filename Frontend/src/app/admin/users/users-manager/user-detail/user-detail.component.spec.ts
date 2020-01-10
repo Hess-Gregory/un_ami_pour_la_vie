85d5e5896548a772e95ee246acb2f2fb331264c5
Gregory <gsweet1981@live.be>
2020-01-10 09:37:51 +0100

    User Details - Module Finished - v.1.2.0

---
+++ Frontend/src/app/admin/users/users-manager/user-detail/user-detail.component.spec.ts
@@ -0,0 +1,30 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { UserDetailComponent } from './user-detail.component';
+import { HttpClient } from '@angular/common/http';
+
+describe('UserDetailComponent', () => {
+  let component: UserDetailComponent;
+  let fixture: ComponentFixture<UserDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [UserDetailComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [HttpClient]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
